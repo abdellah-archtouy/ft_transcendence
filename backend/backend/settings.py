eb31fcee25ed6e5c2f4a235377064e4428478e43
TalalMiftah <talalmiftahvrry@gmail.com>
2024-06-24 20:32:26 +0100

    add readme

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,18 +86,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ft_trans',
         'USER': 'talal',
         'PASSWORD': '200244',
-        # 'NAME': os.environ.get('POSTGRES_DB'),
-        # 'USER': os.environ.get('POSTGRES_USER'),
-        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
