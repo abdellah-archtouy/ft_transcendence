2506643c0d816ce61b4a2cbb00f97855ad206479
Talal Miftah <tmiftah@e3r9p14.1337.ma>
2024-07-13 14:24:29 +0100

    register the models

--- backend/User/admin.py
+++ backend/User/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Achievement, Friend, Notification
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Achievement)
+admin.site.register(Friend)
+admin.site.register(Notification)
