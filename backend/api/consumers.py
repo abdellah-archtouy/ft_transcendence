d37745b12aa010f7a737317779a0f87b82e67524
Abdellah Archtouy <aarchtou@e3r10p14.1337.ma>
2024-07-22 13:55:08 +0100

    ina lah awda hmad

--- backend/api/consumers.py
+++ backend/api/consumers.py
@@ -1,41 +1,59 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 import json
+from .serializer import MessageSerializer , Conversation ,UserSerializer
+from Chat.models import Message , Conversation
+from User.models import User
+from asgiref.sync import async_to_sync
+from django.db import transaction
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        # self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
-        # self.room_group_name = f'chat_{self.conversation_id}'
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
-        # await self.channel_layer.group_add(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
+        self.room_name = self.scope['url_route']['kwargs']['conversation_id']
+        self.room_group_name = f'chat_{self.room_name}'
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
 
-
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         pass
-        # await self.channel_layer.group_discard(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
 
-    async def receive(self, text_data):
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json['message']
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
-        print(text_data)
+        # print(text_data)
+        message_i = text_data_json.get('message', '')
+        conversation_id = text_data_json.get('conversation', '')
+        user_id = text_data_json.get('user', '')
+        if isinstance(text_data_json, dict):
+            msg = Message(conversation_id=conversation_id, user_id=user_id, message=message_i)
+            msg.save()
 
-    async def chat_message(self, event):
+        # Update the last message in the conversation
+            try:
+                with transaction.atomic():
+                    conversation = Conversation.objects.filter(id=conversation_id).first()
+                    if conversation:
+                        conversation.last_message = message_i
+                        conversation.save()
+                        print(f"Updated conversation last message: {conversation.last_message}")
+                    else:
+                        print(f"Conversation with id {conversation_id} not found")
+            except Exception as e:
+                print(f"Error updating conversation: {e}")
+        else:
+            print("Received data is not a dictionary")
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': latest_message_data
+            }
+        )
+
+    def chat_message(self, event):
         message = event['message']
-
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(message)
--- frontend/src/component/Chat/Msg.js
+++ frontend/src/component/Chat/Msg.js
@@ -4,7 +4,7 @@ import Set from './icons/set'
 import Imoji from './icons/imoji';
 import PlayInv from './icons/play_inv'
 import Sent from './icons/sent'
-import ReconnectingWebSocket from 'reconnecting-websocket';
+// import ReconnectingWebSocket from 'reconnecting-websocket';
 import SenderBox from './sender_box';
 import axios from 'axios';
 
@@ -36,16 +36,18 @@ const Chat = ({data1}) => {
     
     const [ws, setWs] = useState(null);
     useEffect(() => {
+        
         const socket = new WebSocket(`ws://localhost:8000/ws/api/msg/${conversationId}/`);
-    
         socket.onopen = () => {
           console.log('WebSocket connection established');
         };
+
     
         socket.onmessage = (e) => {
-          const data = JSON.parse(e.data);
-          setData(prevMessages => [...prevMessages, data.message]);
+          const data1 = JSON.parse(e.data);
+          setData(data => [...data, data1]);
           console.log('data:', data)
+          console.log('data1:', data1)
         };
     
         socket.onclose = () => {
@@ -57,53 +59,29 @@ const Chat = ({data1}) => {
         return () => {
           socket.close();
         };
-      }, []);
+      }, [data]);
 
     if (loading) return <div>Loading...</div>;  
     if (error) return <div>Error: {error.message}</div>;  
 
 
-    // const handleSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const data1 = {
-    //         conversation: 1,
-    //         user: 1,
-    //         message: message,
-    //         time: new Date().toISOString(),
-    //     }
-    //     axios.post('http://localhost:8000/api/post/msg/', data1, {
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     })
-    //     .then(response => {
-    //     console.log(response.data);
-    //     setMessage('');
-    //     })
-    //     .catch(error => {
-    //     if (error.response) {
-    //         console.error('Error response data:', error.response.data);
-    //         console.error('Error response status:', error.response.status);
-    //         console.error('Error response headers:', error.response.headers);
-    //     } else if (error.request) {
-    //         console.error('Error request:', error.request);
-    //     } else {
-    //         console.error('Error message:', error.message);
-    //     }
-    //     });
-
-    //   };
-
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         if (ws) {
-            ws.send(JSON.stringify({ message }));
+            const msg = {
+                conversation: conversationId,
+                user: 1,
+                message: message,
+                conversation_info: data[data.length - 1].conversation_info,
+            };
+            ws.send(JSON.stringify(msg));
             setMessage('');
         }
     };
-
-  return (
-    <div className='Msg'>
+    
+    return (
+        <div className='Msg'>
         <div className='chat_top_bar'>
             <div className='icon_name'>
                 <img src={data[0].conversation_info.uid2_info.avatar} alt='avatar'></img>
@@ -116,14 +94,14 @@ const Chat = ({data1}) => {
         <div className='conversation'>
             
             {data.length === 0 ? (
-            <div className='empty'>
+                <div className='empty'>
                 <p >Add a person <br/>
                 and start a conversation</p>
             </div>
             ) : (
-            data.map(user => (
-                <SenderBox name={user.user === 1 ? ('sender') : ('reciver')} data={user}/>
-            ))
+                data.map(user => (
+                    <SenderBox name={user.user === 1 ? ('sender') : ('reciver')} data={user}/>
+                ))
             )}
             
         </div>
@@ -133,7 +111,7 @@ const Chat = ({data1}) => {
             <PlayInv/>
             {/* <div class="search-container">
                 <input type="text"  placeholder="Enter your message" name="search" class="search-input"></input>
-            </div>
+                </div>
             <Sent/> */}
             <form onSubmit={handleSubmit} class="search-container">
                 <textarea class="search-input"
@@ -141,7 +119,7 @@ const Chat = ({data1}) => {
                     onChange={(e) => setMessage(e.target.value)}
                     placeholder="Enter your message"
                     required
-            />
+                    />
             {/* <Sent className="button" type="submit"/> */}
             <button type="submit"><Sent></Sent></button>
             </form>
@@ -150,4 +128,34 @@ const Chat = ({data1}) => {
   )
 }
 
+// const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const data1 = {
+//         conversation: 1,
+//         user: 1,
+//         message: message,
+//         time: new Date().toISOString(),
+//     }
+//     axios.post('http://localhost:8000/api/post/msg/', data1, {
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     })
+//     .then(response => {
+//     console.log(response.data);
+//     setMessage('');
+//     })
+//     .catch(error => {
+//     if (error.response) {
+//         console.error('Error response data:', error.response.data);
+//         console.error('Error response status:', error.response.status);
+//         console.error('Error response headers:', error.response.headers);
+//     } else if (error.request) {
+//         console.error('Error request:', error.request);
+//     } else {
+//         console.error('Error message:', error.message);
+//     }
+//     });
+
+//   };
 export default Chat
